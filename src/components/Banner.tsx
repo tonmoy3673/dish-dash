7076a3305c57f4cf09ccba545199283f797286a8
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-10-07 18:39:16 +0600

    :art: set banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,8 +10,17 @@ const Banner = () => {
         backgroundRepeat: "no-repeat",
         
       }}
-      className="mt-2 rounded-md"
-    ></div>
+      className="mt-2 rounded-md flex justify-center items-center"
+    >
+        {/* =============== banner title ============ */}
+      <div className="">
+      <h2 className="text-white text-2xl md:text-4xl">Welcome to Dish Dash
+        
+      </h2>
+      <p> Fast. Fresh. Delicious.</p>
+      </div>
+
+    </div>
   );
 };
 
