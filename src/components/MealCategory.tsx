4f6f49928f730a0a9676145bc574538248b61ca2
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-10-14 18:57:03 +0600

    :zap: implements debounce

--- src/components/MealCategory.tsx
+++ src/components/MealCategory.tsx
@@ -3,6 +3,7 @@ import type { MealCategoryType } from "../types/meal-category.types";
 import { getMealCategory } from "../services/meal-category-service";
 import Loader from "../hooks/useLoader";
 import Categories from "./Categories";
+import { LoaderCircle } from "lucide-react";
 
 const MealCategory = () => {
   const [mealCategories, setMealCategories] = useState<MealCategoryType[]>([]);
@@ -10,7 +11,7 @@ const MealCategory = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  
+  const [debounceSearch, setDebounceSearch] = useState<string>("");
 
   // ============== Load FoodCategories ===========//
 
@@ -34,23 +35,34 @@ const MealCategory = () => {
     fetchCategories();
   }, []);
 
+  // ============ debounceSearch ============//
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceSearch(searchTerm.trim().toLowerCase());
+    }, 500);
+    return () => clearTimeout(handler);
+  }, [searchTerm]);
+
   // ============= Filtered list =================//
   const filteredCategories = mealCategories.filter((category) =>
-    category.strCategory.toLowerCase().includes(searchTerm.trim().toLowerCase())
+    category.strCategory.toLowerCase().includes(debounceSearch)
   );
 
   // ============ Which list to render ===========//
   const categoriesToShow =
-    searchTerm.trim() === ""
+    debounceSearch === ""
       ? filteredCategories.slice(0, count)
       : filteredCategories;
 
+  // ============== handleLoad =========//
+  const handleLoad = () => {
+    setCount((pre) => pre + 3);
+  };
 
-    // ============== handleLoad =========//
-    const handleLoad =()=>{
-      setCount((pre)=> pre+3)
-    }
-
+  // ========== handleReset ========//
+  const handleReset =()=>{
+    
+  }
 
   return (
     <div className="py-5 md:py-10">
@@ -70,40 +82,51 @@ const MealCategory = () => {
           className="border px-4 py-2 rounded-md w-72 focus:border-1 focus:border-green-400 focus:outline-none focus:ring focus:ring-green-400"
         />
       </div>
-      {
-        loading ? (
-          <Loader/>
-        ) : error ? (
-          <p className="text-red-500 font-semibold text-xl text-center py-3">{error}</p>
-        ) : categoriesToShow.length > 0 ? (
-          <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <p className="text-red-500 font-semibold text-xl text-center py-3">
+          {error}
+        </p>
+      ) : categoriesToShow.length > 0 ? (
+        <>
           <div className="flex flex-wrap gap-8 justify-center">
             {categoriesToShow.map((category) => (
               <Categories key={category.idCategory} category={category} />
             ))}
           </div>
 
-            {/* ========== Load More Button ========= */}
+          {/* ========== Load More Button ========= */}
 
-            {
-              searchTerm.trim() === '' && count < filteredCategories.length && (
+          {debounceSearch.trim() === "" && (
+            <div className="flex justify-center gap-4 pt-2 md:pt-8">
+              {count < mealCategories.length ? (
                 <div className="flex justify-center pt-5 md:pt-8">
-              <button
-                onClick={handleLoad}
-                className="px-6 py-2 cursor-pointer bg-green-600 text-white font-semibold rounded-md shadow-md hover:bg-amber-600 transition hover:scale-105 duration-300"
-              >
-                Load More
-              </button>
+                  <button
+                    onClick={handleLoad}
+                    className="px-6 flex justify-center items-center gap-x-2 py-2 cursor-pointer bg-green-600 text-white font-semibold rounded-md shadow-md hover:bg-amber-600 transition hover:scale-105 duration-300"
+                  >
+                 <LoaderCircle />   Load More
+                  </button>
+                </div>
+              ) : (
+                <div className="flex justify-center pt-5 md:pt-8">
+                  <button
+                    onClick={handleReset}
+                    className="px-6 py-2 cursor-pointer bg-green-600 text-white font-semibold rounded-md shadow-md hover:bg-amber-600 transition hover:scale-105 duration-300"
+                  >
+                    Reset Category
+                  </button>
+                </div>
+              )}
             </div>
-              )
-            }
-          </>
-        ) : (
-          <p className="text-center text-gray-500 font-semibold text-2xl mt-6">
-            No Data Found.
-          </p>
-        )
-      }
+          )}
+        </>
+      ) : (
+        <p className="text-center text-gray-500 font-semibold text-2xl mt-6">
+          No Data Found.
+        </p>
+      )}
     </div>
   );
 };
