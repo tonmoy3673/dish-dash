0f6e5a6ee947b7e3208fc0b5d63bb68a6182e2ee
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-10-14 17:48:47 +0600

    :bulb: set search input

--- src/components/MealCategory.tsx
+++ src/components/MealCategory.tsx
@@ -1,20 +1,109 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { MealCategoryType } from "../types/meal-category.types";
-
+import { getMealCategory } from "../services/meal-category-service";
+import Loader from "../hooks/useLoader";
+import Categories from "./Categories";
 
 const MealCategory = () => {
-  const [mealCategories,setMealCategories] = useState<MealCategoryType[]>([]);
-  const [count,setCount] = useState<number>(6);
-  const [searchTerm,setSearchTerm] = useState<string>("");
-
+  const [mealCategories, setMealCategories] = useState<MealCategoryType[]>([]);
+  const [count, setCount] = useState<number>(6);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   
 
   // ============== Load FoodCategories ===========//
 
+  useEffect(() => {
+    const fetchCategories = async (): Promise<void> => {
+      try {
+        const response = await getMealCategory();
+        setMealCategories(response);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error(`Failed to fetch categories ${err.message}`);
+          setError(err.name);
+        } else {
+          console.error(`Unknown Error Found ${err}`);
+          throw new Error(`Unknown `);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  // ============= Filtered list =================//
+  const filteredCategories = mealCategories.filter((category) =>
+    category.strCategory.toLowerCase().includes(searchTerm.trim().toLowerCase())
+  );
+
+  // ============ Which list to render ===========//
+  const categoriesToShow =
+    searchTerm.trim() === ""
+      ? filteredCategories.slice(0, count)
+      : filteredCategories;
+
+
+    // ============== handleLoad =========//
+    const handleLoad =()=>{
+      setCount((pre)=> pre+3)
+    }
+
 
   return (
-    <div>
-      
+    <div className="py-5 md:py-10">
+      <div className="mt-2">
+        <h2 className="text-xl md:text-2xl font-semibold text-gray-700 text-center pb-4 raleway">
+          Our Food Categories
+        </h2>
+      </div>
+
+      {/* ============= search Field =======  */}
+      <div className="flex justify-center mb-5">
+        <input
+          type="text"
+          placeholder="Search Category ..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border px-4 py-2 rounded-md w-72 focus:border-1 focus:border-green-400 focus:outline-none focus:ring focus:ring-green-400"
+        />
+      </div>
+      {
+        loading ? (
+          <Loader/>
+        ) : error ? (
+          <p className="text-red-500 font-semibold text-xl text-center py-3">{error}</p>
+        ) : categoriesToShow.length > 0 ? (
+          <>
+          <div className="flex flex-wrap gap-8 justify-center">
+            {categoriesToShow.map((category) => (
+              <Categories key={category.idCategory} category={category} />
+            ))}
+          </div>
+
+            {/* ========== Load More Button ========= */}
+
+            {
+              searchTerm.trim() === '' && count < filteredCategories.length && (
+                <div className="flex justify-center pt-5 md:pt-8">
+              <button
+                onClick={handleLoad}
+                className="px-6 py-2 cursor-pointer bg-green-600 text-white font-semibold rounded-md shadow-md hover:bg-amber-600 transition hover:scale-105 duration-300"
+              >
+                Load More
+              </button>
+            </div>
+              )
+            }
+          </>
+        ) : (
+          <p className="text-center text-gray-500 font-semibold text-2xl mt-6">
+            No Data Found.
+          </p>
+        )
+      }
     </div>
   );
 };
